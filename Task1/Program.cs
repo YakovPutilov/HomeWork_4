457d65cb7ed05f7fee66157d136444e7b8c286aa
Yakov Putiliov <jack27@ya.ru>
2023-11-30 23:27:13 +0300

    Complite Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,27 @@
 ﻿//Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 //Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 
+
+void Request_number(){
+    Console.WriteLine("Enter number: ");
+    string number = Console.ReadLine();
+    if (number == "q"){
+        Console.WriteLine();
+    }
+    else{
+        int num = Convert.ToInt32(number);
+        int count = 0;
+        while(num > 0){
+            count = count + num % 10;
+            num = num / 10;
+        }
+        if (count % 2 != 0){
+            Request_number();
+        }
+        else{
+            Console.WriteLine();
+        }
+    }
+
+}
+Request_number();
