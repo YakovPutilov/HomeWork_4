01e69b1011ce4d51b41f907cdef474f79611702c
Yakov Putiliov <jack27@ya.ru>
2023-12-01 22:36:19 +0300

    Complite Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,43 @@
 ﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив 
 //(первый элемент станет последним, второй – предпоследним и т.д.)
+
+int [] CreateArray(int min, int max, int size)
+{
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array; 
+}
+
+void ShowArray(int [] array)
+{
+    for(int i = 0; i<array.Length;i++)
+    {
+        Console.Write(array[i]+" ");
+    }
+    Console.WriteLine();
+}
+
+void ShowRevArray(int [] array)
+{    
+    for(int i = 0; i<array.Length;i++)
+    {
+        Console.Write(array[array.Length-i-1]+" ");
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine("Enter array size");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter array min");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter array max");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int [] newarray = CreateArray(min, max, size);
+ShowArray(newarray);
+ShowRevArray(newarray);
